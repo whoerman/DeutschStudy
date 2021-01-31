7ba539518febea709856a9bc29587a7d24d97200
Walter Hoerman <whoerman@gmail.com>
2021-01-31 11:30:58 -0500

    changing data

---
+++ data/cbg.js
@@ -0,0 +1,83 @@
+let sentenceArray = [
+    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Bist du fertig?",
+        English: "Are you ready?"
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Hallo!",
+        English: "Hello!"
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Auf Weidersehen!",
+        English: "Goodbye! (Until I see you again)"
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Tschüß!",
+        English: "Bye! (informal)"
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Bis Morgen",
+        English: "See you tomorrow! (Until the morning)"
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Guten Morgen!",
+        English: "Good Morning!"
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Wie geht's?",
+        English: "How are you? (informal how is it going)"
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Wie geht es Ihnen?",
+        English: "How are you? (formal)"
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Danke!",
+        English: "Thanks!"
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Mir geht's gut.",
+        English: "I am well. (to me goes it good)"
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Mir geht's sehr gut.",
+        English: "I am very well,"
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Mir geht's schlecht.",
+        English: "I am bad."
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Mir geht's nicht so gut.",
+        English: "I am not that good."
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Mir geht's wunderbar!",
+        English: "I am wonderful!"
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Mir geht's ausgezeichnet!",
+        English: "I am excellent!"
+    },    {
+        CBG: 1.01,
+        Category: 'Saying how you are feeling',
+        German: "Bis bald!",
+        English: "See you soon!"
+    }, 
+]
